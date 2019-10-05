95122b00f90ddf0efd8e293fb84e8a4a321b6421
Bernd Brägelmann <bernd@braegelmann.net>
2019-10-05 22:15:31 +0200

    Message parsing works

--- bulletinscreen.c
+++ bulletinscreen.c
@@ -1,12 +1,19 @@
+#include <stdbool.h>
 #include <argp.h>
 #include <netdb.h> 
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+
+// TODO
+// Range/Callsign filter
+
+  struct arguments arguments;
 
 const char *config_file_name =
   "~/.bulletinscreen";
@@ -82,6 +89,34 @@ parse_opt (int key, char *arg, struct argp_state *state)
 /* Our argp parser. */
 static struct argp argp = { options, parse_opt, args_doc, doc };
 
+void trimTrailing(char * str)
+{
+  int index, i;
+
+  /* Set default index */
+  index = -1;
+
+  /* Find last index of non-white space character */
+  i = 0;
+  while(str[i] != '\0')
+    {
+      if(str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
+        {
+	  index= i;
+        }
+
+      i++;
+    }
+  /* Mark next character to last non-white space character as NULL */
+  str[index + 1] = '\0';
+}
+
+// valid?
+bool valid_callsign(unsigned char *str){
+  // TODO NEXT
+  return(true);
+}
+
 // For socket
 void error(char *msg)
 {
@@ -89,11 +124,51 @@ void error(char *msg)
   exit(0);
 }
 
+void process(char *buffer){
+  if (buffer[0] == '#') { return; }
+  int i;
+  unsigned char sender[30]; // TODO 7+1 byte should suffice, however i see longer sender callsigns.
+  unsigned char addressee[30];
+  unsigned char msg[100]; // TODO
+  if (arguments.verbose) printf("%s",buffer);
+
+  //parse sender
+  for(i=0;i<strlen(buffer);i++){
+    if (buffer[i]=='>'){
+      strncpy(sender,buffer,i);
+      sender[i]='\0';
+      break;
+    }
+  }
+
+  if (!valid_callsign(sender)){
+    return;
+  }
+  //parse message + addressee
+  char *r_position_ptr = strrchr(buffer, ':');
+  int r_position = (r_position_ptr == NULL ? -1 : r_position_ptr - buffer);
+  memcpy(msg, &buffer[r_position+1], strlen(buffer)-(r_position+1) );
+  msg[strlen(buffer)-(r_position+1)] = '\0';
+
+  for(i=0;i<strlen(msg);i++){
+    //    printf("%i: %02x\n",i,msg[i]);
+    if (msg[i]==(char)0x0d || msg[i]=='{'){
+      msg[i]='\0';
+      break;
+    }
+  }
+  
+  memcpy(addressee,&buffer[r_position-9], 9);
+  addressee[9]='\0';
+  trimTrailing(addressee);
+  printf("%s -> %s: '%s'\n",sender,addressee,msg);
+    
+  
+}
 
 int
 main (int argc, char **argv)
 {
-  struct arguments arguments;
 
   /* Default values. */
   arguments.silent = 0;
@@ -111,52 +186,56 @@ main (int argc, char **argv)
   struct hostent *server;
 
   char buffer[256];
-
-    
   /* Parse our arguments; every option seen by parse_opt will
      be reflected in arguments. */
 
   argp_parse (&argp, argc, argv, 0, 0, &arguments);
+  while(1) {
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+      error("ERROR opening socket");
+    server = gethostbyname(arguments.server);
+    if (server == NULL) {
+      fprintf(stderr,"ERROR, no such host\n");
+      exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, 
+	  (char *)&serv_addr.sin_addr.s_addr,
+	  server->h_length);
+    serv_addr.sin_port = htons(arguments.port);
+    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
+      error("ERROR connecting");
 
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (sockfd < 0) 
-    error("ERROR opening socket");
-  server = gethostbyname(arguments.server);
-  if (server == NULL) {
-    fprintf(stderr,"ERROR, no such host\n");
-    exit(0);
-  }
-  bzero((char *) &serv_addr, sizeof(serv_addr));
-  serv_addr.sin_family = AF_INET;
-  bcopy((char *)server->h_addr, 
-	(char *)&serv_addr.sin_addr.s_addr,
-	server->h_length);
-  serv_addr.sin_port = htons(arguments.port);
-  if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
-    error("ERROR connecting");
-
-  // read ack string from host
-  bzero(buffer,256);
-  n = read(sockfd,buffer,255);
-  if (n < 0) 
-    error("ERROR reading from socket");
-  if (arguments.verbose)
-    printf("%s\n",buffer);
-
- 
-  // user -1 pass -1 vers bulletinscreen 0.0.1 filter t/m
-  bzero(buffer,256);
-  snprintf(buffer, sizeof(buffer), "user -1 pass -1 vers %s filter t/m\n", argp_program_version);
-  n = write(sockfd,buffer,strlen(buffer));
-  if (n < 0) 
-    error("ERROR writing to socket");
-
-  while (1){
+    // read ack string from host
     bzero(buffer,256);
     n = read(sockfd,buffer,255);
-    if (n < 0)
+    if (n < 0) 
       error("ERROR reading from socket");
-    printf("%s\n",buffer);
+    if (arguments.verbose)
+      printf("%s\n",buffer);
+
+ 
+    // user -1 pass -1 vers bulletinscreen 0.0.1 filter t/m
+    bzero(buffer,256);
+    snprintf(buffer, sizeof(buffer), "user -1 pass -1 vers %s filter t/m\n", argp_program_version);
+    if (arguments.verbose) {
+      printf("Sending to server: '%s'\n",buffer);
+    }
+    n = write(sockfd,buffer,strlen(buffer));
+    if (n < 0) 
+      error("ERROR writing to socket");
+
+    n=1;
+    while (n>=0){
+      bzero(buffer,256);
+      n = read(sockfd,buffer,255);
+      process(buffer);
+      //printf(">%s<\n",buffer);
+    }
+    close(sockfd);
+    error("ERROR reading from socket");
   }
   return 0;
 }
